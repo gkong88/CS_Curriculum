f9cfa05a6ff14cd754c372a9366d41c6fc7f033d
George Ong <gkong88@gmail.com>
2019-06-19 14:13:46 -0700

    Add new struct memory_block

--- _Header_Computer_Systems_UCB_CS162/Homework/hw3/mm_alloc.h
+++ _Header_Computer_Systems_UCB_CS162/Homework/hw3/mm_alloc.h
@@ -11,3 +11,11 @@
 void *mm_malloc(size_t size);
 void *mm_realloc(void *ptr, size_t size);
 void mm_free(void *ptr);
+
+typedef struct memory_block {
+    size_t size;
+    struct memory_block *prev;
+    struct memory_block *next;
+    int free;
+    void *mem_pointer;
+} *s_mem_block_ptr;
