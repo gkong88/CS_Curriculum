af9896c385b35e799fbb2f6c9d78c85dfa969e89
George Ong <gkong88@gmail.com>
2019-06-19 14:14:48 -0700

    OS Class: HW3: Write method specs

--- _Header_Computer_Systems_UCB_CS162/Homework/hw3/mm_alloc.c
+++ _Header_Computer_Systems_UCB_CS162/Homework/hw3/mm_alloc.c
@@ -6,6 +6,87 @@
 
 #include "mm_alloc.h"
 #include <stdlib.h>
+#include <unistd.h>
+
+
+s_mem_block_ptr head = NULL;
+s_mem_block_ptr tail = NULL;
+
+/*
+ * Memory block init
+ */
+s_mem_block_ptr mem_block_init(s_mem_block_ptr ptr) {
+    ptr->next = NULL;
+}
+
+/*
+ * Attempts to split the memory block located at s_mem_block_ptr
+ * if it has enough space.
+ *
+ * Otherwise, does nothing. 
+ */
+void split_block(s_mem_block_ptr block_ptr, size_t size) {
+    if (block_ptr->size - size <= sizeof(s_mem_block_ptr)) {
+        return; // not enough extra space to make it worth while
+    }
+    else {
+        s_mem_block_ptr new_block_ptr = block_ptr + size + sizeof(struct memory_block);
+        new_block_ptr -> mem_pointer = new_block_ptr + sizeof(struct memory_block);
+        new_block_ptr -> size = block_ptr -> size - size - sizeof(struct memory_block);
+        new_block_ptr -> prev = block_ptr;
+        new_block_ptr -> free = 1;
+        new_block_ptr -> next = block_ptr -> next;
+        if (new_block_ptr -> next != NULL) {
+            new_block_ptr -> next -> prev = new_block_ptr;
+        }
+        block_ptr -> next = new_block_ptr;
+        block_ptr -> size = size;
+    }
+}
+
+/*
+ * searches existing memory blocks for a free block of size_t size and returns it.
+ *
+ * If none exists, returns NULL
+ */
+void *first_fit(size_t size) {
+    s_mem_block_ptr block_ptr = head;
+    while (block_ptr != NULL) {
+        if (block_ptr->size >= size) {
+            return split_block(block_ptr);
+        }
+        else {
+            block_ptr = block_ptr->next;
+        }
+    }
+    return NULL;
+}
+
+/*
+ * Expands heap memory to fit a new block of size_t.
+ * Returns a pointer to start of new block
+ *
+ * Updates tail reference as necessary.
+ */
+void *get_new_block(size_t size) {
+    void *mem_pointer = sbrk(size + sizeof(struct memory_block)); // alloc memory in the heap
+    if (mem_pointer == -1) {
+        return NULL; // failed, sbrk cannot extend heap further!
+    }
+    s_mem_block_ptr new_block_ptr = (s_mem_block_ptr) mem_pointer; // put your metadata struct at the start of it
+    new_block_ptr -> free = 1; // fill in your meta data
+    new_block_ptr -> mem_pointer = mem_pointer + sizeof(struct memory_block);
+    new_block_ptr -> next = NULL;
+    new_block_ptr -> prev = tail;
+    new_block_ptr -> size = size;
+    if (tail == NULL) {
+        head = new_block_ptr;
+    } else {
+        tail -> next = new_block_ptr;
+    }
+    tail = new_block_ptr;
+    return new_block_ptr;
+}
 
 /*
  * Allocates memory of size_t size
@@ -26,6 +107,12 @@ void *mm_malloc(size_t size) {
      * If first-fit fails, creates more space as necessary.
      */
     /* YOUR CODE HERE */
+    s_mem_block_ptr free_block_ptr;
+
+    free_block_ptr = first_fit(head);
+    if (free_block_ptr == NULL) {
+        free_block_ptr = get_new_block(size_t size);
+    }
     return NULL;
 }
 
